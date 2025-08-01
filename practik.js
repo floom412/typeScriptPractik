a6571b08e7905c4a2cabc4957c1661666192367f
floom412 <vladfloom52747@gmail.com>
2025-08-01 14:49:51 +0300

    add html file

--- practik.js
+++ practik.js
@@ -1,2 +1,5 @@
-var orders = [42, 12, 15, 16, 19];
+"use strict";
+let orders = [42, 12, 15, 16, 19];
 console.log(orders);
+console.log("hello");
+console.log("bye");
--- practik.ts
+++ practik.ts
@@ -1,3 +1,5 @@
 let orders:number[] = [42,12,15,16,19]
 console.log(orders);
 console.log("hello");
+console.log("bye");
+
